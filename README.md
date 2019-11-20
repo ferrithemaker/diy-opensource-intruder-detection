361debee51a24b5ec5e9b3ced8a186bcccff1407
Ferran <ferri.fc@gmail.com>
2019-11-20 22:11:13 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # diy-opensource-intruder-detection
+
 DIY opensource intruder detection device based on Raspberry Pi, Pi CAM, PIR sensor and Telegram
+
+## Components
+
+To build this device you need:
+
+- Raspberry Pi (3+)
+
+- PiCam or USB Webcam (i'm using a standard CSI camera connector PiCam)
+
+- PIR Sensor (i'm using this one -> https://www.elecfreaks.com/estore/pir-sensor-brick.html)
+
+- Telegram app
+
+## Basic building instructions
+
+- Install raspbian distro, with Python3 and Telepot and PiCamera modules using pip3 or apt 
+
+- Install Telegram and create a bot using BotFather and following the instructions
+
+- Install PiCam and PIR Sensor hardware on your Raspberry Pi
+
+- Check if all is working separately
+
+- Set the python script telegram key and chat id from BotFather telegram data   
+
+- Run the python script
+
+# Basic usage instructions
+
---
+++ intruder-detector.py
@@ -0,0 +1,60 @@
+
+import time
+import telepot
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+
+camera = PiCamera()
+camera.rotation = 180
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.IN)
+intruder = False
+enabled = False
+
+def handle(msg):
+    global enabled
+    global intruder
+    
+    command = msg['text']
+    from_id = msg['from']['id']
+    #chat_id = msg['chat']['id']
+    #print 'Got command: %s' % command
+    if from_id == 0000000: # your id from Telegram
+    	if command.lower() == 'show':
+        	camera.start_preview()
+        	time.sleep(5)
+        	camera.capture('/home/pi/image.jpg')
+        	camera.stop_preview()
+        	inf = open('/home/pi/image.jpg', 'rb')
+        	#bot.sendMessage(chat_id,text="Done!")
+        	bot.sendPhoto(chat_id,inf)
+    	if command.lower() == "enable pir":
+    		enabled = True
+    		bot.sendMessage(chat_id,text="PIR enabled")
+    	if command.lower() == "disable pir":
+    		enabled = False
+    		bot.sendMessage(chat_id,text="PIR disabled")
+    else:
+    	bot.sendMessage(from_id,text="I'm not allowed to talk with you, sorry")
+    	bot.sendMessage(chat_id,text="Somebody is trying to use the chatbot")
+
+chat_id = xxxxxxx
+bot = telepot.Bot('xxxxxxxxx')
+bot.message_loop(handle)
+#print 'I am listening...'
+
+while 1:
+    pirvalue = GPIO.input(11)
+    if pirvalue == 1 and intruder == False and enabled == True:
+        intruder = True
+        camera.start_preview()
+        time.sleep(5)
+        camera.capture('/home/pi/image.jpg')
+        camera.stop_preview()
+        inf = open('/home/pi/image.jpg')
+        bot.sendPhoto(chat_id,inf)
+    if pirvalue == 0:
+    		intruder = False
+    time.sleep(1)
